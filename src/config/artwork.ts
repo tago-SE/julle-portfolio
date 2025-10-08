ad8bb077eac2d20e6a75a9d7546b4cb92b871569
Tiago Redaelli <tiago.redaelli@if.se>
2025-10-09 00:03:30 +0200

    julle artwork update

--- src/config/artwork.ts
+++ src/config/artwork.ts
@@ -1,6 +1,3 @@
-import art1 from "@/assets/artwork/art-1.jpg";
-import art2 from "@/assets/artwork/art-2.jpg";
-import art3 from "@/assets/artwork/art-3.jpg";
 import coinflip2 from "@/assets/artwork/coinflip2.jpg";
 import coverall from "@/assets/artwork/coverall.jpg";
 import cutout from "@/assets/artwork/cutout.jpg";
@@ -9,24 +6,6 @@ import flowers from "@/assets/artwork/flowers.jpg";
 import inoutdocumentation from "@/assets/artwork/inoutdocumentation.jpg";
 
 const artworks = [
-  {
-    id: 1,
-    image: art1,
-    title: "Reflection of Fire",
-    year: "2024",
-  },
-  {
-    id: 2,
-    image: art2,
-    title: "Ethereal Valley",
-    year: "2024",
-  },
-  {
-    id: 3,
-    image: art3,
-    title: "Color Grid Dreams",
-    year: "2024",
-  },
   {
     id: 5,
     image: coinflip2,
