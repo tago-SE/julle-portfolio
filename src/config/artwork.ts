1e854dc62f5f5dabba791b979949a79114e4aa1d
Tiago Redaelli <tiago.redaelli@if.se>
2025-10-08 23:59:08 +0200

    added julles images to scroller

--- src/config/artwork.ts
+++ src/config/artwork.ts
@@ -1,7 +1,12 @@
 import art1 from "@/assets/artwork/art-1.jpg";
 import art2 from "@/assets/artwork/art-2.jpg";
 import art3 from "@/assets/artwork/art-3.jpg";
-import art4 from "@/assets/artwork/geko.jpg";
+import coinflip2 from "@/assets/artwork/coinflip2.jpg";
+import coverall from "@/assets/artwork/coverall.jpg";
+import cutout from "@/assets/artwork/cutout.jpg";
+import cutout2 from "@/assets/artwork/cutout2.png";
+import flowers from "@/assets/artwork/flowers.jpg";
+import inoutdocumentation from "@/assets/artwork/inoutdocumentation.jpg";
 
 const artworks = [
   {
@@ -23,10 +28,40 @@ const artworks = [
     year: "2024",
   },
   {
-    id: 4,
-    image: art4,
-    title: "Geko",
-    year: "2023",
+    id: 5,
+    image: coinflip2,
+    title: "Coin Flip",
+    year: "2024",
+  },
+  {
+    id: 6,
+    image: coverall,
+    title: "Cover All",
+    year: "2024",
+  },
+  {
+    id: 7,
+    image: cutout,
+    title: "Cutout",
+    year: "2024",
+  },
+  {
+    id: 8,
+    image: cutout2,
+    title: "Cutout 2",
+    year: "2024",
+  },
+  {
+    id: 9,
+    image: flowers,
+    title: "Flowers",
+    year: "2024",
+  },
+  {
+    id: 10,
+    image: inoutdocumentation,
+    title: "In/Out Documentation",
+    year: "2024",
   },
 ];
 
